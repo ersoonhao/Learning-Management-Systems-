4e595ec390b478bab8739ac1070fcb70d1861a7b
ersoonhao <soonhao.er@gmail.com>
2021-09-19 19:04:04 +0800

    template
    sss

--- server.js
+++ server.js
@@ -1,10 +1,82 @@
-var express = require('express');
-var port = process.env.PORT || 3000;
-var app = express(),
-path = require('path'),
-publicDir = path.join(__dirname,'public');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-app.use(express.static(publicDir))
+const path = __dirname + '/app/views/';
 
-app.listen(port);
-module.exports = app;
+const app = express();
+
+app.use(express.static(path));
+
+
+// Cross-origin resource sharing (CORS) defines a way for client web applications that are loaded in one domain to interact with resources in a different domain. 
+// var corsOptions = {
+//   origin: "*"
+// };
+
+// var corsOptions = {
+//     origin: "api.akashic.technology/#/"
+//   };
+
+
+// app.use(cors(corsOptions));
+
+app.use(cors({
+    methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+}));
+
+
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const db = require("./app/models");
+
+// added cors for aws?? 
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*')
+//     res.header('Access-Control-Allow-Credentials', true)
+//     res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+//     next();
+//   });
+
+
+
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Credentials', true)
+    res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    next()
+})
+
+
+
+
+
+db.sequelize.sync();
+// // drop the table if it already exists
+// db.sequelize.sync({ force: true }).then(() => {
+//   console.log("Drop and re-sync db.");
+// });
+
+app.get('/', function (req,res) {
+  console.log(path);
+  res.sendFile(path + "index.html");
+});
+
+require("./app/routes/turorial.routes")(app);
+
+
+// figure the ports 
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+// const PORT =  8081;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
