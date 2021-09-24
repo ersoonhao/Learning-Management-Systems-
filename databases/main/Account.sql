568461f07a317bef73294dbb4d2c886865756a8a
wafturerobin-dev <wafturerobin-dev@users.noreply.github.com>
2021-09-24 14:34:22 +0800

    Account

---
+++ databases/main/Account.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `Account` (
+  `accountId` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) NOT NULL,
+  `isAdmin` tinyint DEFAULT '0',
+  `isTrainer` tinyint DEFAULT '0',
+  `password` varchar(100) DEFAULT NULL,
+  `dateCreated` datetime DEFAULT NULL,
+  `dateUpdated` datetime DEFAULT NULL,
+  PRIMARY KEY (`accountId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
