36795c4b515ce32e939cb3a86b0de3e91838d167
wafturerobin-dev <wafturerobin-dev@users.noreply.github.com>
2021-09-24 14:42:00 +0800

    DB init update

--- databases/main/Class.sql
+++ databases/main/Class.sql
@@ -5,7 +5,7 @@ CREATE TABLE `Class` (
   `selfEnrollStartDateTime` datetime NOT NULL,
   `selfEnrollEndDateTime` datetime NOT NULL,
   `maxCapacity` int NOT NULL,
-  `trnUserRoleId` int NOT NULL,
-  `adminUserRoleId` int NOT NULL,
+  `trnAccountId` int NOT NULL,
+  `adminAccountId` int NOT NULL,
   PRIMARY KEY (`classId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
