ae2995ba5f66be249a700b1b66c21396d76f373e
johncheong1234 <john.cheong.2019@smu.edu.sg>
2021-09-28 18:34:54 +0800

    commented out old test code

--- test/test.js
+++ test/test.js
@@ -1,10 +1,10 @@
 'use strict';
 
-const supertest = require('supertest'); 
-const test = require('unit.js');
-const app = require('../server.js');
+// const supertest = require('supertest'); 
+// const test = require('unit.js');
+// const app = require('../server.js');
 
-const request = supertest(app);
+// const request = supertest(app);
 
 // describe('Tests app', function() {
 //   it('verifies get', function(done) {
