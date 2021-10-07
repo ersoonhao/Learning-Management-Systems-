c71280f2006b96713d234c7ae7edd7bdf4372011
johncheong1234 <john.cheong.2019@smu.edu.sg>
2021-10-07 15:19:19 +0800

    Update a course instance test code

--- test/unit/course_test.js
+++ test/unit/course_test.js
@@ -1,4 +1,4 @@
-// CONTRIBUTOR: JohnCheong
+// CONTRIBUTOR: John Cheong
 
 const assert = require('assert')
 const { Course } = require("../../app/models");
@@ -17,7 +17,30 @@ describe('Course Creation', ()=>{
         const c = Object.assign({}, course);
             
         const result = Course.createCourse(c, c.title,c.description,c.active);
+        
+        assert(result != 'Physics');
         console.log(result)
-        assert(result != null);
+    })
+
+
+})
+
+describe('Course Update', ()=>{
+    before(function(){
+        course = {
+            courseId: null,
+            title: "Physics",
+            description: "A course on Physics",
+            active: false
+        }
+    });
+
+    it('Update a course Instance', ()=>{
+        const c = Object.assign({}, course);
+            
+        const result = Course.updateCourse(c, 'Chemistry','A course on chemistry',false);
+        
+        assert(result.title == 'Chemistry');
+        console.log(result)
     })
 })
