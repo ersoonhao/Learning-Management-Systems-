f5c5f755d803185eb4465bdbe8c04a30f73b9497
johncheong1234 <john.cheong.2019@smu.edu.sg>
2021-10-23 21:53:52 +0800

    Test, routes and controller, for prerequisite sets
    Test code, routes and controllers for retrieving sets of prerequisite courses, added name to test code as well

---
+++ app/controllers/prerequisiteSet.controller.js
@@ -0,0 +1,61 @@
+const db = require("../models");
+const PrerequisiteSet = db.PrerequisiteSet;
+const Op = db.Sequelize.Op;
+
+exports.create = (req,res) =>{
+    if(!req.body){
+        res.status(400).send({
+            message: "Request body is empty!"
+        })
+        return
+    }
+
+    const prerequisiteSet = {
+      setNumber: req.body.setNumber,
+      course_fk: req.body.course_fk
+    }
+
+    PrerequisiteSet.create(prerequisiteSet)
+    .then(data=>{
+        res.send(data)
+    }).catch(err=>{
+        res.status(500).send({
+            message:
+            err.message || "Some error occured while creating the Message"
+        })
+    })
+}
+
+exports.findAll = (req, res) => {
+
+  PrerequisiteSet.findAll({ where: {
+
+  } })
+    .then(data => {
+      res.send(data); //change this to render
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving PrerequisiteSet."
+      });
+    });
+};
+
+exports.findAllByCourseFK = (req, res) => {
+
+    const course_fk = req.body.course_fk;
+    
+    PrerequisiteSet.findAll({ where: {
+      course_fk : course_fk
+    }})
+      .then(data => {
+        res.send(data); //change this to render
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while retrieving Messages."
+        });
+      });
+  };
