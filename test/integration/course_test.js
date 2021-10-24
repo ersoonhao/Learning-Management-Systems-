e14f49f4c632347cb174caa37527a7cd1712ec03
johncheong1234 <john.cheong.2019@smu.edu.sg>
2021-10-24 09:39:14 +0800

    more comprehensive test to test for prerequisites

--- test/integration/course_test.js
+++ test/integration/course_test.js
@@ -30,6 +30,28 @@ describe('The courses route and controller',()=>{
     )
   })
 
+
+  it('retrieves course 1 with prerequisites through get request',(done)=>{
+    request(app).get('/api/course/find/1').end(
+      (err,response)=>{
+        assert(response.body.course.title == 'Physics')
+        assert(response.body.course.description == 'This course is about Physics')
+        assert(response.body.course.active == true)
+        assert(response.body.course.CoursePrerequisites.length == 1)
+        assert(response.body.course.CoursePrerequisites[0].setNumber == 1)
+        assert(response.body.course.CoursePrerequisites[0].courseId == 1)
+        assert(response.body.course.CoursePrerequisites[0].PrerequisiteSets.length == 3)
+        assert(response.body.course.CoursePrerequisites[0].PrerequisiteSets[0].setNumber==1)
+        assert(response.body.course.CoursePrerequisites[0].PrerequisiteSets[0].course_fk==2)
+        assert(response.body.course.CoursePrerequisites[0].PrerequisiteSets[1].setNumber==1)
+        assert(response.body.course.CoursePrerequisites[0].PrerequisiteSets[1].course_fk==3)
+        assert(response.body.course.CoursePrerequisites[0].PrerequisiteSets[2].setNumber==1)
+        assert(response.body.course.CoursePrerequisites[0].PrerequisiteSets[2].course_fk==4)
+        done()
+      }
+    )
+  })
+
   it('retrieves all courses through get request', (done)=>{
     request(app).get('/api/course').end(
     (err,response)=>{
