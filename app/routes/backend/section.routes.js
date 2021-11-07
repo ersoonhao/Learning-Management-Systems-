52d125abf7874d7b75e5f294d973922ec41661f3
asher935 <78128377+asher935@users.noreply.github.com>
2021-11-08 01:38:57 +0800

    update section controller

---
+++ app/routes/backend/section.routes.js
@@ -0,0 +1,21 @@
+module.exports = app => {
+    ROUTE_PATH = "/api/section"
+    var router = require("express").Router();
+
+    //======== START: CONTROLLER LOGIC ========
+    const section = require("../../controllers/section.controller");
+
+
+    router.get("/allsections", section.findAllSection);
+    router.post("/getall", section.getSectionPackage);
+    router.get("/:id", section.findOne);
+
+    router.post("/", section.createSection);
+    router.post("/delete", section.deleteSection);
+    router.post('/update', section.updateSection);
+    router.post("/deleteall", section.deleteAllSection);
+
+    //======== END: CONTROLLER LOGIC ========
+
+    app.use(ROUTE_PATH, router);
+}
