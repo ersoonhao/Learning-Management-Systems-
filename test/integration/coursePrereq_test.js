8baf05a630a83315461ec9ba42930fd08f16e2a6
johncheong1234 <john.cheong.2019@smu.edu.sg>
2021-11-08 01:02:00 +0800

    return of the course prereq integration test

--- test/integration/coursePrereq_test.js
+++ test/integration/coursePrereq_test.js
@@ -1,3 +1,31 @@
+// CONTRIBUTOR: John Cheong
+'use strict';
+
+const request = require('supertest'); 
+const app = require('../../server.js');
+const assert = require('assert')
+
+const dummy_reload = require("../../app/dummy/reload")
+
+describe('The courses prerequisites route and controller',()=>{
+  before(function(done){
+      dummy_reload.reload().then(() => { done() })
+  })
+
+  it('deletes one prerequisite set through post request', (done)=>{
+    request(app).post('/api/courseprereq/delete').send({session: dummy_reload.SESSION_ADMIN_TRAINER,"courseId":1, "setNumber":2}).end(
+    (err,response)=>{
+      console.log(response.body)
+      assert.equal(response.body.message,"set was deleted successfully!")
+      done()
+    
+    }
+    )
+  })
+
+})
+
+
 // // // CONTRIBUTOR: John Cheong
 // 'use strict';
 
@@ -12,17 +40,7 @@
 //     dummy_reload.reload().then(() => { done() })
 //   })
 
-//   it('deletes one prerequisite set through post request', (done)=>{
-//     request(app).post('/api/courseprereq/delete').send({session: dummy_reload.SESSION_ADMIN_TRAINER,"courseId":1, "setNumber":2}).end(
-//     (err,response)=>{
-//       console.log(response.body)
-//       assert.equal(response.body.message,"set was deleted successfully!")
-//       done()
-    
-//     }
-//     )
-//   })
+  
 
 
 // })
-
