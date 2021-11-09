ee470fa14268466c35b41e2c24858cb0c836e2ea
asher935 <78128377+asher935@users.noreply.github.com>
2021-11-09 10:23:15 +0800

    fixed isEligible

--- app/controllers/enrollment.controller.js
+++ app/controllers/enrollment.controller.js
@@ -29,10 +29,13 @@ exports.isEligibleForCourse = (req, res) => {
                         include: {
                             model: PrerequisiteSet,
                             include: {
-                                model: Class,
+                                model: Course,
                                 include: {
-                                    model: Enrollment,
-                                    where: { accountId: session.accountId, coursePassed: true }
+                                    model: Class,
+                                    include: {
+                                        model: Enrollment,
+                                        where: { accountId: session.accountId, coursePassed: true }
+                                    }
                                 }
                             }
                         }
@@ -205,17 +208,17 @@ exports.getMyEnrolledClasses = (req, res) => {
             }
 
             Enrollment.findAll({
-                where: stmt,
-                include: [ { model: Class, include: [Course] } ]
-            })
-            .then(data => {
-                res.send({ enrollment: data })
-            })
-            .catch(err => {
-                res.status(500).send({
-                    message: err.message || 'Some error occured obtaining data'
+                    where: stmt,
+                    include: [{ model: Class, include: [Course] }]
                 })
-            })
+                .then(data => {
+                    res.send({ enrollment: data })
+                })
+                .catch(err => {
+                    res.status(500).send({
+                        message: err.message || 'Some error occured obtaining data'
+                    })
+                })
         }
     })
 
